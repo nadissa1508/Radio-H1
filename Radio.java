e1ea553e42f926ecf1a6f001301591fd04f8adad
Nadissa Vela <122498954+nadissa1508@users.noreply.github.com>
2024-01-14 00:55:36 -0600

    Agregación funcionalidad nextStation, validación para que la estacion no sobrepase los límites solicitados

--- Radio.java
+++ Radio.java
@@ -2,12 +2,14 @@ public class Radio {
 
     private boolean frequency;
     private boolean status;
+    private double actualStation;
     private double[] stations; 
 
     public Radio(){
         //AM -> true FM -> false
         frequency = true;
         status = true;
+        actualStation = 530.0;
         stations = new double[12];
     }
 
@@ -24,7 +26,27 @@ public class Radio {
         }
     }
 
-    
+    double nextStation(){
+        double station = 0.0, add = 0.0, range = 0.0;
+
+        if(isAM()){
+            add = 10;
+            range = 1610;
+        }else{
+            add = 0.2;
+            range= 107.9;
+        }
+
+        station += add;
+
+        if((station > range) && (add == 10)){
+            station = 530;
+        }else if((station > range) && (add == 107.9)){
+            station = 87.9;
+        }
+
+        return station;
+    }    
 
     
 }
