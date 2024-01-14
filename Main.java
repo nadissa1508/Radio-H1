e7de7c0451fdc76b2eb941c79291ef3084ebf3e9
Nadissa Vela <122498954+nadissa1508@users.noreply.github.com>
2024-01-14 00:02:23 -0600

    Se colocó la función del radio que se debe ejecutar según las opciones del menu

--- Main.java
+++ Main.java
@@ -34,20 +34,82 @@ public class Main {
 
             if(option >0 && option < 7){
                 if(option == 1){
-                    System.out.println("\n1");
+                    //analizar como cambiar el estado sin utilizar parámetros
+                    radio.switchOnOff();                 
 
                 }else if(option == 2){
-                    System.out.println("\n2");
+                    if(!radio.isOn()){  
+                        System.out.println("\nError, ingrese debe encender el radio");
+                    }else{
+                        radio.switchAMFM();
+                    }
 
                 }else if(option == 3){
-                    System.out.println("\n3");
+                    if(!radio.isOn()){
+                        System.out.println("\nError, ingrese debe encender el radio");
+                    }else{
+                        double station = radio.nextStation();
+                        System.out.println("\nEsta escuchando la emisora " + station +" !");
+                    }
 
                 }else if(option == 4){
-                    System.out.println("\n4");
+                    if(!radio.isOn()){
+                        System.out.println("\nError, ingrese debe encender el radio");
+                    }else{
+                        String id = "", estacion = "";
+                        int buttonId = 0;
+                        double station = 0.0;
+                        
+                        System.out.println("\nIngrese número de botón para la estación: ");
+                        id = teclado.nextLine();
+
+                        try{
+                            buttonId = Integer.parseInt(id);
+                        }catch(Exception e){
+                            System.out.println("\nError, ingrese un número");
+                            return;
+                        }
+
+                        System.out.println("\nIngrese la estación que desea guardar: ");
+                        estacion = teclado.nextLine();
+
+                        try{
+                            station = Double.parseDouble(estacion);
+                        }catch(Exception e){
+                            System.out.println("\nError, ingrese un número decimal");
+                            return;
+                        }
+
+                        radio.saveStation(buttonId, station);
+                    }
 
                 }else if(option == 5){
-                    System.out.println("\n5");
+                    if(!radio.isOn()){
+                        System.out.println("\nError, ingrese debe encender el radio");
+                    }else{
+                        String id = "";
+                        int buttonId = 0;
 
+                        System.out.println("\nIngrese el número de botón de la estación: ");
+                        id = teclado.nextLine();
+
+                        try{
+                            buttonId = Integer.parseInt(id);
+                        }catch(Exception e){
+                            System.out.println("\nError, ingrese un número");
+                            return;
+                        }
+
+                        double station = radio.selectStation(buttonId);
+                        System.out.println("\nReproduciendo la emisora " + station +" !");
+                    }
+
+                }else if(option == 6){
+                    if(!radio.isOn()){
+                        System.out.println("\nError, el radio ya está apagado");
+                    }else{
+                        radio.switchOnOff();
+                    }
                 }
             }else{
                 System.out.println("\nError, opción incorrecta");
