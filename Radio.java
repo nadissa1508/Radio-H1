d44aec5dbdfab08a5ea991a6954d4d479c513a37
Nadissa Vela <122498954+nadissa1508@users.noreply.github.com>
2024-01-14 00:13:37 -0600

    Agregación de atributos clase Radio y funcionalidad del método isOn

--- Radio.java
+++ Radio.java
@@ -1,3 +1,18 @@
 public class Radio {
+
+    private boolean frequency;
+    private boolean status;
+    private double[] stations; 
+
+    public Radio(){
+        frequency = true;
+        status = true;
+        stations = new double[12];
+    }
+
+    boolean isOn(){
+        return status;
+    }
+
     
 }
