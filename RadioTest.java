9d06acc1ef4489e41944812bc95eec9f2908b770
miafuentes30 <fue23775@uvg.edu.gt>
2024-01-14 20:14:32 -0500

    nuevas pruebas unitarias del codigo

--- RadioTest.java
+++ RadioTest.java
@@ -6,7 +6,8 @@ public class RadioTest {
 
     private Radio radio;
 
-    // Este método se ejecuta antes de cada prueba
+
+    // método que se ejecuta antes de cada prueba
     @Before
     public void setUp() {
         radio = new Radio();
@@ -16,14 +17,52 @@ public class RadioTest {
     @Test
     public void testSelectStationWhenRadioOff() {
         radio.switchOnOff(); // Apaga el radio
-        double selectedStation = radio.selectStation(1); // Intenta seleccionar una estación
-        assertEquals(-1, selectedStation, 0.01); // Verifica que devuelva -1 cuando el radio está apagado
+        double selectedStation = radio.selectStation(1); //seleccionar  estación
+        assertEquals(-1, selectedStation, 0.01);
     }
 
     // Prueba para verificar que devuelve -1 cuando el número de botón es inválido
     @Test
     public void testSelectStationWithInvalidButton() {
-        double selectedStation = radio.selectStation(0); // Intenta seleccionar una estación con un botón inválido
-        assertEquals(-1, selectedStation, 0.01); // Verifica que devuelva -1 cuando el número de botón es inválido
+        double selectedStation = radio.selectStation(0); //seleccionar una estación con un botón inválido
+        assertEquals(-1, selectedStation, 0.01);
     }
+
+     // guardar estacion en el que se encuentra el radio
+    @Test
+    public void testSelectStation() {
+        radio.switchOnOff(); // Enciende el radio
+        radio.switchAMFM();  // Cambia a AM
+        radio.saveStation(1, 600); // Guarda una estación en el botón 
+        double result = radio.selectStation(1);
+        assertEquals(600.0, result, 0.01); 
+    }
+
+    // Prueba para el método switchOnOff cuando el radio está apagado
+    @Test
+    public void testSwitchOnOffRadioApagado() {
+        Radio radio = new Radio();
+        assertFalse(radio.isOn()); // Aseguramos que el radio está apagado 
+        radio.switchOnOff(); //encender
+        assertTrue(radio.isOn()); // Verificamos que el radio esté encendido
+    }
+
+    // Prueba para el método switchOnOff cuando el radio está encendido
+    @Test
+    public void testSwitchOnOffRadioEncendido() {
+        Radio radio = new Radio();
+        radio.switchOnOff(); //ACtualmente el radio esta encendido
+        assertTrue(radio.isOn()); // Apagar 
+        radio.switchOnOff();
+        assertFalse(radio.isOn()); // Verificamos que el radio esté apagado después de llamar al método
+    }
+
+    // Prueba cuando el botón es inválido
+    @Test
+    public void testSaveStation_invalidButtonId() {
+        int invalidButtonId = 15;
+        double station = 101.5;
+        radio.saveStation(invalidButtonId, station); // Verifica que el mensaje de error sea el esperado
+    }
+
 }
