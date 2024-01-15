da241a3a313bff5c044a0ca9c879503d35311656
Nadissa Vela <122498954+nadissa1508@users.noreply.github.com>
2024-01-15 16:10:44 -0600

    Agregación de un test para el metodo NextStation, comprueba funcionamiento del dial

--- RadioTest.java
+++ RadioTest.java
@@ -1,6 +1,7 @@
 import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
 public class RadioTest {
 
@@ -11,14 +12,6 @@ public class RadioTest {
     @Before
     public void setUp() {
         radio = new Radio();
-    }
-
-    // Prueba para verificar que devuelve -1 cuando el radio está apagado
-    @Test
-    public void testSelectStationWhenRadioOff() {
-        radio.switchOnOff(); // Apaga el radio
-        double selectedStation = radio.selectStation(1); //seleccionar  estación
-        assertEquals(-1, selectedStation, 0.01);
     }
 
     // Prueba para verificar que devuelve -1 cuando el número de botón es inválido
@@ -32,10 +25,18 @@ public class RadioTest {
     @Test
     public void testSelectStation() {
         radio.switchOnOff(); // Enciende el radio
-        radio.switchAMFM();  // Cambia a AM
+        radio.switchAMFM();  // Cambia a FM
         radio.saveStation(1, 600); // Guarda una estación en el botón 
         double result = radio.selectStation(1);
         assertEquals(600.0, result, 0.01); 
+    }
+
+    // Prueba para guardar una estación cuando el botón es inválido
+    @Test
+    public void testSaveStation_invalidButtonId() {
+        int invalidButtonId = 15;
+        double station = 101.5;
+        radio.saveStation(invalidButtonId, station); // Verifica que el mensaje de error sea el esperado
     }
 
     // Prueba para el método switchOnOff cuando el radio está apagado
@@ -57,12 +58,14 @@ public class RadioTest {
         assertFalse(radio.isOn()); // Verificamos que el radio esté apagado después de llamar al método
     }
 
-    // Prueba cuando el botón es inválido
+    // Prueba para verficar el avance del dial en las emisoras
     @Test
-    public void testSaveStation_invalidButtonId() {
-        int invalidButtonId = 15;
-        double station = 101.5;
-        radio.saveStation(invalidButtonId, station); // Verifica que el mensaje de error sea el esperado
+    public void testNextStationWorksCorrectly() {
+        radio.switchOnOff(); // Enciende el radio
+        radio.saveStation(1, 1610);
+        radio.selectStation(1);
+        double inicialStation = radio.nextStation();
+        Assertions.assertEquals(530, inicialStation);
     }
 
 }
